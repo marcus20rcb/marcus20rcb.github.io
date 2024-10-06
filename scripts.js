7733aecb69db2c4ef493ddff9910de5d7c86fe79
Marcus Vinicius Ovando <marcus20rcb@gmail.com>
2024-10-06 00:33:17 -0400

    teste

--- scripts.js
+++ scripts.js
@@ -1,24 +1,48 @@
-var meses = ["Setembro","Outubro","Novembro","Dezembro","Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro"];
-    var planejadas = [16,10,13,38,41,49,35,35,34];
-    var executadas = [13,10,7,29,35,34,31,35,33];
-    var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: meses,
-            datasets: [
-                { 
-                    data: planejadas,
-                    label: "planejadas",
-                    borderColor: "#3e95cd",
-                    fill: false
-                },
-                { 
-                    data: executadas,
-                    label: "executadas",
-                    borderColor: "#3e95cd",
-                    fill: false
-                }
-            ]   
-        }
-    });
+// Our labels along the x-axis
+var years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
+// For drawing the lines
+var africa = [86,114,106,106,107,111,133,221,783,2478];
+var asia = [282,350,411,502,635,809,947,1402,3700,5267];
+var europe = [168,170,178,190,203,276,408,547,675,734];
+var latinAmerica = [40,20,10,16,24,38,74,167,508,784];
+var northAmerica = [6,3,2,2,7,26,82,172,312,433];
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: years,
+    datasets: [
+      { 
+        data: africa,
+        label: "Africa",
+        borderColor: "#3e95cd",
+        fill: false
+      },
+      { 
+        data: asia,
+        label: "Asia",
+        borderColor: "#8e5ea2",
+        fill: false
+      },
+      { 
+        data: europe,
+        label: "Europe",
+        borderColor: "#3cba9f",
+        fill: false
+      },
+      { 
+        data: latinAmerica,
+        label: "Latin America",
+        borderColor: "#e8c3b9",
+        fill: false
+      },
+      { 
+        data: northAmerica,
+        label: "North America",
+        borderColor: "#c45850",
+        fill: false
+      }
+    ]
+  }
+});
