aef3613c4d5ccf9c26911498f60f59d82ca72a50
Marcus Vinicius Ovando <marcus20rcb@gmail.com>
2024-10-19 17:26:01 -0400

    add formulario contato

---
+++ processar_formulario.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $mensagem = $_POST['mensagem'];
+
+    $dados = "Nome: $nome\nEmail: $email\nMensagem: $mensagem\n\n";
+
+    $arquivo = fopen("dados_formulario.txt", "a");
+    fwrite($arquivo, $dados);
+    fclose($arquivo);
+
+    echo "Dados salvos com sucesso!";
+} else {
+    echo "Método de requisição inválido.";
+}
+?>
